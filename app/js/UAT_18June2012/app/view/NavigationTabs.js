7cdff82eb63e9c1812e56e839e5b89c4c88a90b8
Mike Barlow <mike.barlow@dbitpro.com>
2013-12-20 11:34:49 -0500

    Fix for Issue #126 - Rename "Treatment" Panel and "Admin"Tab Also performed code cleanup and JSLint check

--- app/js/UAT_18June2012/app/view/NavigationTabs.js
+++ app/js/UAT_18June2012/app/view/NavigationTabs.js
@@ -1,51 +1,40 @@
 Ext.define('COMS.view.NavigationTabs' ,{
-	extend: 'Ext.tab.Panel',
-	alias : 'widget.NavigationTabs',
-	name : 'Main Navigation Tabs',
+    extend: 'Ext.tab.Panel',
+    alias : 'widget.NavigationTabs',
+    name : 'Main Navigation Tabs',
 
-	plain : true,
+    plain : true,
 
-		// MWB - 5/28/2012 - Moved down to the initComponent so that we can control at load time who sees what tabs
-//	items : [
-//		{ title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
-//		,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
-//		,{ title : 'Template Authoring', items : [ { xtype : 'AuthoringTab'} ] }
-//		,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
-//		,{ title : 'Admin', items : [ {xtype : 'AdminTab' }]}	// KD - 12/20/11 - Added new Tab  to PUT data back to PHP
-//	],
-	initComponent: function() {
-		wccConsoleLog("Navigation Tabs View - Initialization");
+    initComponent: function() {
+        wccConsoleLog("Navigation Tabs View - Initialization");
 
-		// Based on the "Sessionrole" set in main.php ($role = $_SESSION['role'];)
-		// determine who can see what tabs.
-		// The same process can be used to show/hide various other elements such as buttons 
-		if ("Administrator" === Sessionrole || "All Roles" === Sessionrole) {
-			this.items = [
-				{ title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
-				,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
-				,{ title : 'Template Authoring', items : [ { xtype : 'AuthoringTab'} ] }
-				,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
-				,{ title : 'Admin', items : [ {xtype : 'AdminTab' }]}	// KD - 12/20/11 - Added new Tab  to PUT data back to PHP
-			];
-		}
-		else if ("1" === SessionTemplateAuthoring) {
-			this.items = [
-				{ title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
-				,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
-				,{ title : 'Template Authoring', items : [ { xtype : 'AuthoringTab'} ] }
-				,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
-			];
-		}
-		else {
-			this.items = [
-				{ title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
-				,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
-				,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
-			];
-		}
-
-
-
-		this.callParent(arguments);
-	}
+        // Based on the "Sessionrole" set in main.php ($role = $_SESSION['role'];)
+        // determine who can see what tabs.
+        // The same process can be used to show/hide various other elements such as buttons 
+        if ("Administrator" === Sessionrole || "All Roles" === Sessionrole) {
+            this.items = [
+                { title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
+                ,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
+                ,{ title : 'Template Authoring', items : [ { xtype : 'AuthoringTab'} ] }
+                ,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
+                ,{ title : 'Site Configuration', items : [ {xtype : 'AdminTab' }]}
+            ];
+        }
+        else if ("1" === SessionTemplateAuthoring) {
+            this.items = [
+                { title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
+                ,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
+                ,{ title : 'Template Authoring', items : [ { xtype : 'AuthoringTab'} ] }
+                ,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
+            ];
+        }
+        else {
+            this.items = [
+                { title : 'Patient',  items : [ { xtype : 'NewPlanTab' } ] }
+                ,{ title : 'Orders', items : [ { xtype : 'OrdersTab'} ] }
+                ,{ title : 'Messages', items : [ { xtype : 'MessagesTab' } ] }
+            ];
+        }
+        this.callParent(arguments);
+    }
 });
--- app/js/UAT_18June2012/app/view/NewPlan/CTOS/NursingDocs/Treatment.js
+++ app/js/UAT_18June2012/app/view/NewPlan/CTOS/NursingDocs/Treatment.js
@@ -1,155 +1,143 @@
-/*jslint undef: true, unparam: true, sloppy: true, vars: true, white: true, maxerr: 50, indent: 4 */
 Ext.define("COMS.view.NewPlan.CTOS.NursingDocs.Treatment_Meds", {
-	extend: "Ext.grid.Panel",
-	alias : "widget.NursingDocs_Treatment_Meds",
+    extend: "Ext.grid.Panel",
+    alias : "widget.NursingDocs_Treatment_Meds",
 
-	store : "ND_Treatment",
+    store : "ND_Treatment",
 
-	autoScroll : true,
-	columnLines : true,
-	sortableColumns : false,
-	enableColumnHide : false,
-	enableColumnMove : false,
+    autoScroll : true,
+    columnLines : true,
+    sortableColumns : false,
+    enableColumnHide : false,
+    enableColumnMove : false,
     features: [{ftype:'grouping'}],
 
-	selType: "cellmodel",
-	plugins: [
-		// Event handler for this is assigned to the "beforeedit" event and calls the "CellEdit()" function in the TreatmentTab controller
-		Ext.create("Ext.grid.plugin.CellEditing", {
-			clicksToEdit: 1
-		})
-	],
+    selType: "cellmodel",
+    plugins: [
+        // Event handler for this is assigned to the "beforeedit" event and calls the "CellEdit()" function in the TreatmentTab controller
+        Ext.create("Ext.grid.plugin.CellEditing", {
+            clicksToEdit: 1
+        })
+    ],
 
 
 
-	initComponent: function() {
-		wccConsoleLog("Treatment Meds Grid - Initialization");
-		var startTimeField = new Ext.form.field.Time( { increment : 1, snapToIncrement : true, id : "startTimeEditor", format : "h:i A" });
-		var endTimeField = new Ext.form.field.Time( { increment : 1, snapToIncrement : true, id : "endTimeEditor", format : "h:i A" });
+    initComponent: function() {
+        wccConsoleLog("Treatment Meds Grid - Initialization");
+        var startTimeField = new Ext.form.field.Time( { increment : 1, snapToIncrement : true, id : "startTimeEditor", format : "h:i A" });
+        var endTimeField = new Ext.form.field.Time( { increment : 1, snapToIncrement : true, id : "endTimeEditor", format : "h:i A" });
 
-		this.columns = [
-		{ header : "", dataIndex : "typeOrder", hidden : true,
-	        renderer: function(value) {
-				switch (value) {
-					case 1:
-						return "Pre Therapy";
-					case 2:
-						return "Therapy";
-					case 3:
-						return "Post Therapy";
-				}
-			}
-		},
-		{ header : "Medication", dataIndex : "drug", width : 120 },
-		{ header : "Dose", dataIndex : "dose", width : 50, editor: { allowBlank: false } },
-		{ header : "Units", dataIndex : "unit", width : 70, editor : { 
-				xtype: "combo", 
-				store: "DrugUnitsStore", 
-				displayField: "name", 
-				valueField: "name" 
-			} 
-		}, 
-		{ header : "Route", dataIndex : "route", width : 50, editor : { 
-				xtype: "combo", 
-				store: "InfusionStore", 
-				displayField: "name", 
-				valueField: "name" 
-			} 
-		},
-		{ header : "Start Time", dataIndex : "StartTime", 
-			renderer: function(v) {
-				if ("" !== v) {
-					var v1, v2, v3;
-					try {
-						v2 = Ext.Date.format(v, "h:i A");
-					}
-					catch (e) {
-						try {
-							v1 = Ext.Date.parse(v, "c");
-							v2 = Ext.Date.format(v1, "h:i A");
-						}
-						catch (e) {
-							wccConsoleLog("Error in time renderer");
-						}
-					}
-					return v2;
-				}
-				return v;
-			},
-			editor : startTimeField
-		},
-		{ header : "End Time", dataIndex : "EndTime", 
-			renderer: function(v) {
-				if ("" !== v) {
-					var v1, v2, v3;
-					try {
-						v2 = Ext.Date.format(v, "h:i A");
-					}
-					catch (e) {
-						try {
-							v1 = Ext.Date.parse(v, "c");
-							v2 = Ext.Date.format(v1, "h:i A");
-						}
-						catch (e) {
-							wccConsoleLog("Error in time renderer");
-						}
-					}
-					return v2;
-				}
-				return v;
-			},
-			editor : endTimeField
-		},
-		{ header : "Comments", dataIndex : "Comments", width : 250, editor : { xtype : "textfield" } },
-		{ header : "Signature", dataIndex : "Treatment_User", renderer: 
-			function(value, metadata, record, rowIndex, colIndex, store, view) {
-				var aStyle = "style=\"text-decoration:underline; color: navy;\" ";
-				var dspValue = "Sign to Verify";
-				if (value) {
-					aStyle = "";
-					dspValue = (value + " - " + record.get("Treatment_Date"));
-				}
-				return Ext.String.format("<span class=\"anchor TreatmentSigner\" {0}row={1} col={2}>{3}</span>", aStyle, rowIndex, colIndex, dspValue);
-			},
-			width : 200
-		}
-
-		
-//		,{ xtype : "actionbuttoncolumn", width : 100, header : "Signature", items : [
-//			{
-//				text : "Sign to Verify",
-//				eventName: "SignTreatment"
-//			}
-//		]}
-	];
-		this.callParent(arguments);
-	}
+        this.columns = [
+        { header : "", dataIndex : "typeOrder", hidden : true,
+            renderer: function(value) {
+                switch (value) {
+                    case 1:
+                        return "Pre Therapy";
+                    case 2:
+                        return "Therapy";
+                    case 3:
+                        return "Post Therapy";
+                }
+            }
+        },
+        { header : "Medication", dataIndex : "drug", width : 120 },
+        { header : "Dose", dataIndex : "dose", width : 50, editor: { allowBlank: false } },
+        { header : "Units", dataIndex : "unit", width : 70, editor : {
+                xtype: "combo",
+                store: "DrugUnitsStore",
+                displayField: "name",
+                valueField: "name"
+            }
+        },
+        { header : "Route", dataIndex : "route", width : 50, editor : {
+                xtype: "combo",
+                store: "InfusionStore",
+                displayField: "name",
+                valueField: "name"
+            }
+        },
+        { header : "Start Time", dataIndex : "StartTime",
+            renderer: function(v) {
+                if ("" !== v) {
+                    var v1, v2, v3;
+                    try {
+                        v2 = Ext.Date.format(v, "h:i A");
+                    }
+                    catch (err1) {
+                        try {
+                            v1 = Ext.Date.parse(v, "c");
+                            v2 = Ext.Date.format(v1, "h:i A");
+                        }
+                        catch (err2) {
+                            wccConsoleLog("Error in time renderer");
+                        }
+                    }
+                    return v2;
+                }
+                return v;
+            },
+            editor : startTimeField
+        },
+        { header : "End Time", dataIndex : "EndTime",
+            renderer: function(v) {
+                if ("" !== v) {
+                    var v1, v2, v3;
+                    try {
+                        v2 = Ext.Date.format(v, "h:i A");
+                    }
+                    catch (err3) {
+                        try {
+                            v1 = Ext.Date.parse(v, "c");
+                            v2 = Ext.Date.format(v1, "h:i A");
+                        }
+                        catch (err4) {
+                            wccConsoleLog("Error in time renderer");
+                        }
+                    }
+                    return v2;
+                }
+                return v;
+            },
+            editor : endTimeField
+        },
+        { header : "Comments", dataIndex : "Comments", width : 250, editor : { xtype : "textfield" } },
+        { header : "Signature", dataIndex : "Treatment_User", renderer:
+            function(value, metadata, record, rowIndex, colIndex, store, view) {
+                var aStyle = "style=\"text-decoration:underline; color: navy;\" ";
+                var dspValue = "Sign to Verify";
+                if (value) {
+                    aStyle = "";
+                    dspValue = (value + " - " + record.get("Treatment_Date"));
+                }
+                return Ext.String.format("<span class=\"anchor TreatmentSigner\" {0}row={1} col={2}>{3}</span>", aStyle, rowIndex, colIndex, dspValue);
+            },
+            width : 200
+        }
+    ];
+        this.callParent(arguments);
+    }
 });
 
 Ext.define("COMS.view.NewPlan.CTOS.NursingDocs.Treatment" ,{
-	extend: "Ext.panel.Panel",
-	alias : "widget.NursingDocs_Treatment",
-	name : "Nursing Documentation Treatment Tab",
-	title: "Treatment",
-	items : [
-		{ xtype : "fieldset",
-			padding : "10",
-			defaultType : "fieldset",
-			items : [
-				{ xtype : "button", text : "Treatment Complete" },
-				{
-					title : "Medication Given",
-					name : "ND_T_Meds",
-					defaults : { margin : "5 0 30 0" },
-					items : [
-					// { xtype : "NursingDocs_Treatment_Meds", title : "Pre Therapy", name : "PreMedsGrid" },
-					// { xtype : "NursingDocs_Treatment_Meds", title : "Therapy", name : "MedsGrid" },
-					{ xtype : "NursingDocs_Treatment_Meds", title : "Treatment Administered", name : "AdministeredMedsGrid" }
-						
-					]
-				},
-				{ xtype : "button", text : "Treatment Complete", hidden : true }
-			]
-		}
-	]
+    extend: "Ext.panel.Panel",
+    alias : "widget.NursingDocs_Treatment",
+    name : "Nursing Documentation Treatment Tab",
+    title: "Administration",
+    items : [
+        { xtype : "fieldset",
+            padding : "10",
+            defaultType : "fieldset",
+            items : [
+                { xtype : "button", text : "Treatment Complete" },
+                {
+                    title : "Medication Given",
+                    name : "ND_T_Meds",
+                    defaults : { margin : "5 0 30 0" },
+                    items : [
+                        { xtype : "NursingDocs_Treatment_Meds", title : "Treatment Administered", name : "AdministeredMedsGrid" }
+                    ]
+                },
+                { xtype : "button", text : "Treatment Complete", hidden : true }
+            ]
+        }
+    ]
 });
