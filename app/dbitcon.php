c0cfcaee01e7023c7f76c8641e4c829924a52d4c
barlowm <barlowm@gmail.com>
2014-04-03 22:18:13 -0400

    Tweak for performance Status - See Issue #30 Performance Status Updates Listed on Vital Signs Historical Table

--- app/dbitcon.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$serverName = "DBITDATA\DBIT";
-	$connectionOptions = array("UID"=>"coms_db_user","PWD"=>"dbitPASS99","Database"=>"COMS_MWB_TEST");
-	$conn =  sqlsrv_connect( $serverName, $connectionOptions);
-	
-?>
