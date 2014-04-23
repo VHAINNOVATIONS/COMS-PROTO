1d5a3f6b82c85420fa079fcf67485e151ee50482
Sean Cassidy <sean.cassidy@dbitpro.com>
2014-04-22 22:43:51 -0400

    Minor improvements to branding

--- app/login.php
+++ app/login.php
@@ -1,9 +1,9 @@
 <body bgcolor="#EFEFEF">
 
 <?php
-echo "<table = '1' border = '1' align='center' width='800'>";
+echo "<table border = '2' align='center' width='100%'>";
 echo "<tr><td align='center' colspan='2'><img src='images/va_logo_v1.png'></td></tr>";
-echo "<tr><td align='center' width='800'>";
+echo "<tr><td align='center' width='75%'>";
 echo "<br>*****<B>TEST SYSTEM</B>******<BR> 
    *************************************************************************<br> 
            You have accessed the CPM  Cache Account and VistaLink is installed in this account<br>
@@ -20,7 +20,7 @@ echo "<br>*****<B>TEST SYSTEM</B>******<BR>
    *************************************************************************<br>
 ";
 echo "</td>";
-echo "<td align='left'>";
+echo "<td align='left' width='25%'>";
 echo "<table><tr><td align='right'><form name='input' action='index.php' method='post' accept-charset='ISO-8859-1'><br>
 Access Code:</td><td align='left'> <input type='password' name='AccessCode' /></td></tr><tr><td></td></tr><tr><td align='right'>Verify Code:</td><td align='left'> <input type='password' name='VerifyCode' /></td></tr><tr><td colspan=2></td></tr><tr><td align='center' colspan=2>	
 <input type='submit' value='&nbsp;Submit&nbsp;' />&nbsp;&nbsp;<input type='reset' value='&nbsp;Clear&nbsp;' />
