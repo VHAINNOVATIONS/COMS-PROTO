d999bb1dc77fe75e96ce200afa0246569925a309
barlowm <barlowm@gmail.com>
2014-05-29 21:51:09 -0400

    Work in progress to address the following issues:     Type of Cancer within Patient Information Panel #40     Patient Designations #135

--- app/js/app/controller/Common/puWinSelCancer.js
+++ app/js/app/controller/Common/puWinSelCancer.js
@@ -6,7 +6,8 @@ Ext.define("COMS.controller.Common.puWinSelCancer", {
 
 	refs: [
 		{ ref: "Disease",		selector: "selDisease"},
-		{ ref: "DiseaseStage",	selector: "selDiseaseStage"}
+		{ ref: "DiseaseStage",	selector: "selDiseaseStage"},
+	{ ref: "DiseaseStageInput",	selector : "[name=\"Select Disease Stage Control\"]" }
 	],
 
 
@@ -27,12 +28,35 @@ Ext.define("COMS.controller.Common.puWinSelCancer", {
 	},
 
 	piDiseaseSelected: function (combo, recs, eOpts) {
-		debugger;
 		this.application.Cancer = recs[0].data;
 		var stage = this.getDiseaseStage();
 		var stageStore = stage.getStore();
 		stageStore.removeAll();
+
+					var stage = this.getDiseaseStage();
+					var theStore = stage.getStore();
+					var theInputField = this.getDiseaseStageInput();
+					stage.setRawValue("");
+					stage.setValue("");
+					stage.clearValue();
+					stage.reset();
+					stage.resetOriginalValue();
+					stage.lastSelection = [];
+
+stage.clearValue();
+stage.applyEmptyText();
+stage.getPicker().getSelectionModel().doMultiSelect([], false);
+var inputEl = stage.inputEl;
+var orig = stage.originalValue;
+var qu = stage.lastQuery;
+var inputTagID = stage.getInputId();
+var submitValue = stage.getSubmitValue()
+var theValue = stage.getValue();
+
+
+
 		stageStore.load({
+			scope : this,
 			params : {
 				URL : Ext.URLs.DiseaseStage + "/",
 				ID  : this.application.Cancer.id
@@ -40,15 +64,39 @@ Ext.define("COMS.controller.Common.puWinSelCancer", {
 			callback : function(records, operation, success) {
 				debugger;
 				if(success){
+					var stage = this.getDiseaseStage();
+					var theStore = stage.getStore();
+					var theInputField = this.getDiseaseStageInput();
 					stage.setRawValue("");
 					stage.setValue("");
+					stage.clearValue();
+					stage.reset();
+					stage.resetOriginalValue();
+					stage.lastSelection = [];
+
+stage.clearValue();
+stage.applyEmptyText();
+stage.getPicker().getSelectionModel().doMultiSelect([], false);
+// stage.bindStore(null);
+
+var inputEl = stage.inputEl;
+var orig = stage.originalValue;
+var qu = stage.lastQuery;
+var inputTagID = stage.getInputId();
+var submitValue = stage.getSubmitValue()
+var theValue = stage.getValue();
+
+
+
+
+
+debugger;
 				}
 			}
 		});
 	},
 
 	onDiseaseStageChange: function (combo, recs, eOpts) {
-		debugger;
 		this.application.Cancer.Stage = recs[0].data;
 	}
 });
