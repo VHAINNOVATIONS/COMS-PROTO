b10f352dfff02c2cdad3eb73dc86466c91ddf51c
barlowm <barlowm@gmail.com>
2014-06-27 07:24:58 -0400

    More Code Cleanup for JSLint validation

--- app/js/app/view/Common/puWinSelCancer.js
+++ app/js/app/view/Common/puWinSelCancer.js
@@ -1,34 +1,28 @@
-Ext.define("COMS.view.Common.puWinSelCancer",{
+Ext.define("COMS.view.Common.puWinSelCancer", {
 	"extend" : "Ext.window.Window",
 	"alias" : "widget.puWinSelCancer",
-	"title" : "Patient Type(s) of Cancer",
-	"name" : "piCancerSelection",
-				closeAction: 'hide',
-				width: 360,
-				height: 270,
-				minHeight: 270,
-				layout: 'fit',
-				resizable: true,
-				modal: true,
+	"title" : "Patient Type of Cancer",
+	// "name" : "puCancerSelection",
+	"closeAction" : "hide",
+	"width" : 360,
+	"height" : 270,
+	"minHeight" : 270,
+	"layout" : "fit",
+	"resizable" : true,
+	"modal" : true,
 	"items" : [
 		{
 			"xtype" : "form",
-			layout: { type: "vbox", align: "stretch" },
-			border: false,
-			bodyPadding: 10,
-			fieldDefaults: {
-				labelAlign: 'top',
-				labelWidth: 100,
-				labelStyle: 'font-weight:bold'
-			},
-			defaults: {
-				margins: '0 0 10 0'
-			},
-
-			items: [{ xtype : "selDisease" }, { xtype : "selDiseaseStage" }],
-			buttons: [
-				{ text: 'Save', scope : this },
-				{ text: 'Cancel' }
+			"layout" : { "type" : "vbox", "align" : "stretch" },
+			"border" : false,
+			"bodyPadding" : 10,
+			"items" : [
+				{ "xtype" : "selDisease" }, 
+				{ "xtype" : "selDiseaseStage" }
+			],
+			"buttons" : [
+				{ "text" : "Save", "scope" : this },
+				{ "text" : "Cancel" }
 			]
 		}
 	]
--- app/js/app/view/Common/selDiseaseStage.js
+++ app/js/app/view/Common/selDiseaseStage.js
@@ -1,16 +1,16 @@
-Ext.define('COMS.view.Common.selDiseaseStage' ,{
-	extend: 'Ext.form.field.ComboBox',
-	alias : 'widget.selDiseaseStage',
-	name : 'Select Disease Stage Control',
+Ext.define("COMS.view.Common.selDiseaseStage" ,{
+	"extend" : "Ext.form.field.ComboBox",
+	"alias" : "widget.selDiseaseStage",
+	"name" : "Select Disease Stage Control",
 	"emptyText" : "Select a Cancer Stage",
 
-	store : 'DiseaseStage',
-	displayField: 'Stage',
-	valueField: 'ID',
-	queryMode: 'local',
-	fieldLabel: 'Cancer Stage',
-	labelAlign: 'right',
-	width: 250,
-	size : 10,
-	labelWidth: 90
+	"store" : "DiseaseStage",
+	"displayField" : "Stage",
+	"valueField" : "ID",
+	"queryMode" : "local",
+	"fieldLabel" : "Cancer Stage",
+	"labelAlign" : "right",
+	"width" : 250,
+	"size " : 10,
+	"labelWidth" : 90
 });
