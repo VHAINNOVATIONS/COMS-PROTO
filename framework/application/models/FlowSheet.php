622e8cf95004725858a0b7376cd28a68fce00c0c
Sean Cassidy <sean.cassidy@dbitpro.com>
2014-09-16 16:04:21 -0400

    Updated Flowsheet
    Updated Flowsheet Call to return the data from Order_Status table

--- framework/application/models/FlowSheet.php
+++ framework/application/models/FlowSheet.php
@@ -425,50 +425,56 @@ class Flowsheet extends Model
 ///newfunction
 function FS($patientID){
         
-
-$query = "SELECT 
-Performance_ID as ph_Performance_ID ,Patient_History.Weight as ph_Weight ,Treatment_ID as ndt_TreatmentID 
-,ndt.Patient_ID as ndt_Patient_ID ,ndt.Template_ID as ndt_Template_ID ,ndt.PAT_ID as ndt_PATID 
-,fspn.PAT_ID as fs_PATID ,ndt.Cycle as ndt_Cycle ,AdminDay as ndt_AdminDay ,ndt.AdminDate as ndt_AdminDate 
-,Type as ndt_Type ,Drug as ndt_Drug ,Dose as ndt_Dose ,Unit as ndt_Unit ,Route as ndt_Route 
-,StartTime as ndt_StartTime ,EndTime as ndt_EndTime ,Comments as ndt_Comments ,Treatment_User as ndt_TreatmentUser 
-,Treatment_Date as ndt_TreatmentDate ,Drug_OriginalValue as ndt_DrugOriginalValue 
-,Dose_OriginalValue as ndt_DoseOriginalValue ,Unit_OriginalValue as ndt_UnitOriginalValue 
-,Route_OriginalValue as ndt_RouteOriginalValue 
-FROM ND_Treatment ndt 
-CROSS JOIN Patient_History
- LEFT JOIN FlowSheet_ProviderNotes fspn ON fspn.PAT_ID = ndt.PAT_ID 
- AND ndt.Patient_ID = '$patientID'
-WHERE Patient_History.Weight IS NOT NULL
-AND Patient_History.Weight != ''";  
-		
-		$result = $this->query($query);
-
-		//echo "Query1: ".$query."<BR>";
-		
-	$query2 = "SELECT TOP 1 Performance_ID as ph_Performance_ID
-		,Patient_History.Weight as ph_Weight,
-		Date_taken 
-		FROM Patient_History
-		Where Patient_ID = '$patientID'
-		AND Weight != ''";
-		
-		$result2 = $this->query($query2);
-
-		//echo "Query2: ".$query2."<BR>";
-		
-		$query3 = "SELECT Template_ID, Order_ID, Order_Status, Drug_Name, Order_Type, FlowRate, AdminDay, Admin_Date, AdminTime, Amt, InfusionTime, Sequence, FluidVol, Reason, Date_Entered
-,RegNum, RegDose, RegDoseUnit, RegDosePct, RegReason, PatientDose, PatientDoseUnit, Route, flvol, flunit, infusion, bsaDose, Reason
+	$query = "SELECT Order_Status
+      ,Drug_Name
+      ,Order_Type
+      ,Template_ID
+      ,Template_IDMT
+      ,Patient_ID
+      ,Order_ID
+      ,Drug_ID
+      ,Date_Modified
+      ,Date_Entered
+      ,Notes
+      ,FluidType
+      ,FluidVol
+      ,FlowRate
+      ,AdminDay
+      ,InfusionTime
+      ,AdminTime
+      ,Sequence
+      ,Amt
+      ,iAmt
+      ,Unit
+      ,Type
+      ,RegNum
+      ,RegDose
+      ,RegDoseUnit
+      ,RegDosePct
+      ,RegReason
+      ,PatientDose
+      ,PatientDoseUnit
+      ,Route
+      ,flvol
+      ,flunit
+      ,infusion
+      ,bsaDose
+      ,Reason
+      ,Admin_Date
+      ,DateApplied
+      ,DateEndedActual
+      ,DateEnded
+      ,Goal
+      ,ClinicalTrial
+      ,PerformanceStatus
+      ,WeightFormula
+      ,BSAFormula
   FROM Order_Status
   WHERE Patient_ID = '$patientID'";
 		
-		$result3 = $this->query($query3);
-		
-		//echo "Query3: ".$query3."<BR>";
-		
-
-		$arr = array_merge ((array)$result,(array)$result2,(array)$result3);
-		//var_dump($arr);
-        return ($arr);
+		$result = $this->query($query);
+		//var_dump($result);
+		//$arr = array_merge ((array)$result,(array)$result2,(array)$result3);
+        return ($result);
     }
 }
