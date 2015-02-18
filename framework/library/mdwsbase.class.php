d52d1f9807b25fb98968ffe20b59dc7044634cdf
Rahul Gokulnath <rahul.gokulnath@turner.com>
2015-02-18 15:21:15 -0500

    Fixed formatting issues

--- framework/library/mdwsbase.class.php
+++ framework/library/mdwsbase.class.php
@@ -4,232 +4,248 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-class MdwsBase {
-    
-    function MDWS_Setup($roles) {
-			$sitelist = $_SESSION['sitelist'];
-			$AC = $_SESSION['AC'];
-			$VC = $_SESSION['VC'];
-            $_SESSION['MDWS_Status'] = '';
-            $_SESSION['MDWS_Type'] = '';
-            $_SESSION['MDWS_Msg'] = '';
-            $_SESSION['MDWS_Suggestion'] = '';
-			//echo "<br>MDWS Dump:".var_dump($_SESSION)."<br>";
+class MdwsBase
+{
+    public function MDWS_Setup($roles)
+    {
+        $sitelist = $_SESSION['sitelist'];
+        $AC = $_SESSION['AC'];
+        $VC = $_SESSION['VC'];
+        $_SESSION['MDWS_Status'] = '';
+        $_SESSION['MDWS_Type'] = '';
+        $_SESSION['MDWS_Msg'] = '';
+        $_SESSION['MDWS_Suggestion'] = '';
+            //echo "<br>MDWS Dump:".var_dump($_SESSION)."<br>";
 
             try {
-                    $client = new SoapClient("http://".$_SESSION['mdws']."/EmrSvc.asmx?WSDL");
-                    if (isset($client->fault)) {
-                            $this->MDWsCrashReport($client, "SoapClient", false);
-                            return null;
-                    }
+                $client = new SoapClient("http://".$_SESSION['mdws']."/EmrSvc.asmx?WSDL");
+                if (isset($client->fault)) {
+                    $this->MDWsCrashReport($client, "SoapClient", false);
 
-/*					
-					$addDataSource = $client->addDataSource(array('id'=>'355','name'=>'vaphsdb04','datasource'=>'54.243.40.32','port'=>'9210','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
+                    return;
+                }
+
+/*
+                    $addDataSource = $client->addDataSource(array('id'=>'355','name'=>'vaphsdb04','datasource'=>'54.243.40.32','port'=>'9210','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
                     if (isset($connect->connectResult->fault)) {
                             $this->MDWsCrashReport($connect->connectResult, "Connect", false);
                             return null;
                     }
 */
-					// $addDataSource = $client->addDataSource(array('id'=>'355','name'=>'I20355VSTLNX','datasource'=>'54.83.44.110','port'=>'9200','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
-					$addDataSource = $client->addDataSource(array('id'=>'355','name'=>'I20355VSTLNX','datasource'=>'54.83.44.110','port'=>'9300','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
-                    if (isset($connect->connectResult->fault)) {
-                            $this->MDWsCrashReport($connect->connectResult, "Connect", false);
-                            return null;
-                    }
+                    // $addDataSource = $client->addDataSource(array('id'=>'355','name'=>'I20355VSTLNX','datasource'=>'54.83.44.110','port'=>'9200','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
+                    $addDataSource = $client->addDataSource(array('id' => '355', 'name' => 'I20355VSTLNX', 'datasource' => '54.83.44.110', 'port' => '9300', 'modality' => 'HIS', 'protocol' => 'VISTA', 'region' => '355'));
+                if (isset($connect->connectResult->fault)) {
+                    $this->MDWsCrashReport($connect->connectResult, "Connect", false);
 
-                    $connect = $client->connect(array('sitelist'=>$sitelist));
-                    if (isset($connect->connectResult->fault)) {
-                            $this->MDWsCrashReport($connect->connectResult, "Connect", false);
-                            return null;
-                    }
+                    return;
+                }
 
-                    $login = $client->login(array('username'=>$AC,'pwd'=>$VC,'context'=>''));
-                    if (isset($login->loginResult->fault)) {
-                            $this->MDWsCrashReport($login->loginResult, "Login", false);
-                            return null;
-                    }
+                $connect = $client->connect(array('sitelist' => $sitelist));
+                if (isset($connect->connectResult->fault)) {
+                    $this->MDWsCrashReport($connect->connectResult, "Connect", false);
 
-                    if (isset($client->fault)) {
-                            $this->MDWsCrashReport($client, "Client", false);
-                            return null;
-                    }
-                    
-                    return $client;
-                    
+                    return;
+                }
+
+                $login = $client->login(array('username' => $AC, 'pwd' => $VC, 'context' => ''));
+                if (isset($login->loginResult->fault)) {
+                    $this->MDWsCrashReport($login->loginResult, "Login", false);
+
+                    return;
+                }
+
+                if (isset($client->fault)) {
+                    $this->MDWsCrashReport($client, "Client", false);
+
+                    return;
+                }
+
+                return $client;
             } catch (Exception $e) {
                 // echo ("Error: $e->getMessage()");
             }
-            return null;
+
+        return;
     }
 
-    function MDWS_Disconnect($client){
-        
+    public function MDWS_Disconnect($client)
+    {
         $disconnect = $client->disconnect();
         if (isset($disconnect->disconnectResult->fault)) {
             $this->MDWsCrashReport($disconnect->disconnectResult, "Disconnect", false);
-            return null;
+
+            return;
         }
-        
+
         $disconnect = $client->disconnectRemoteSites();
         if (isset($disconnect->disconnectRemoteSitesResult->fault)) {
             $this->MDWsCrashReport($disconnect->disconnectRemoteSitesResult, "Disconnect", false);
-            return null;
+
+            return;
         }
-        
     }
 
-    function MDWS_TimestampConvert($date) {
-            // YYYYMMDD.HHMMSS
+    public function MDWS_TimestampConvert($date)
+    {
+        // YYYYMMDD.HHMMSS
             // 012345678901234
             // 000000000011111
             $year = substr($date, 0, 4);
-            $mon = substr($date, 4, 2);
-            $day = substr($date, 6, 2);
-            $hr = substr($date, 9, 2);
-            $min = substr($date, 11, 2);
-            $sec = substr($date, 13, 2);
-            $parsedDate = "$mon/$day/$year $hr:$min:$sec";
+        $mon = substr($date, 4, 2);
+        $day = substr($date, 6, 2);
+        $hr = substr($date, 9, 2);
+        $min = substr($date, 11, 2);
+        $sec = substr($date, 13, 2);
+        $parsedDate = "$mon/$day/$year $hr:$min:$sec";
             // echo "$date - $year - $mon - $day - $hr - $min - $sec<br />";
             return ($parsedDate);
     }
 
-    function MDWS_Convert2JSON($dfn, $rawData) {
-            $array = (array)$rawData;
+    public function MDWS_Convert2JSON($dfn, $rawData)
+    {
+        $array = (array) $rawData;
 
             //Format JSON
             $jsonRecord = array();
-            $jsonRecord['success'] = 'true';
-            $jsonRecord['total'] = count($rawData);
+        $jsonRecord['success'] = 'true';
+        $jsonRecord['total'] = count($rawData);
 
-            $recordArray = array();
-            $recordArray['id'] = $dfn;
-            $recordArray['data'] = $rawData;
+        $recordArray = array();
+        $recordArray['id'] = $dfn;
+        $recordArray['data'] = $rawData;
 
-            $jsonRecord['records'] = $recordArray;
-            return json_encode($jsonRecord); 
+        $jsonRecord['records'] = $recordArray;
+
+        return json_encode($jsonRecord);
     }
 
+    public function MDWs_ShowData($dfn, $rawData, $patientId)
+    {
+        $recordArray = array();
+        $recordArray['id'] = $patientId;
+        $recordArray['dfn'] = $dfn;
+        $recordArray['data'] = $rawData;
 
-    function MDWs_ShowData( $dfn, $rawData, $patientId ) {
-            $recordArray = array();
-            $recordArray['id'] = $patientId;
-            $recordArray['dfn'] = $dfn;
-            $recordArray['data'] = $rawData;
-
-            $data = json_encode($recordArray); 
-            echo $data;
+        $data = json_encode($recordArray);
+        echo $data;
     }
 
-    function MDWsCrashReport($result, $type, $displayReport) {
-            if (isset($result->fault)) {
-                    $fault = $result->fault;
-                    $_SESSION['MDWS_Status'] = 'Crashed';
-                    $_SESSION['MDWS_Msg'] = $fault->message;
-                    $_SESSION['MDWS_Suggestion'] = $fault->suggestion;
-		            $_SESSION['MDWS_Type'] = $type;
-		            //$_SESSION['cprsUsername'] = $cprsUsername;
+    public function MDWsCrashReport($result, $type, $displayReport)
+    {
+        if (isset($result->fault)) {
+            $fault = $result->fault;
+            $_SESSION['MDWS_Status'] = 'Crashed';
+            $_SESSION['MDWS_Msg'] = $fault->message;
+            $_SESSION['MDWS_Suggestion'] = $fault->suggestion;
+            $_SESSION['MDWS_Type'] = $type;
+                    //$_SESSION['cprsUsername'] = $cprsUsername;
                     $this->MDWSCrashed($displayReport);
-                    return (null);
-            }
-            return ($result);
+
+            return;
+        }
+
+        return ($result);
     }
-    
-    function MDWSCrashed($displayReport) {
-			$errMsg = "";
-			if ("" !== $_SESSION['MDWS_Type']) {
-				$errMsg .= "MDWS Type: " . $_SESSION['MDWS_Type'] . "; ";
-			}
-			if ("" !== $_SESSION['MDWS_Msg']) {
-				$errMsg .= "MDWS Msg: " . $_SESSION['MDWS_Msg'] . "; ";
-			}
-			if ("" !== $_SESSION['MDWS_Suggestion']) {
-				$errMsg .= "MDWS Suggestion: " . $_SESSION['MDWS_Suggestion'] . "; ";
-			}
-			if ("" === $errMsg) {
-				$errMsg .= "Unknown MDWS Error; No further details to report";
-			}
-			return ($errMsg);
-		/********
+
+    public function MDWSCrashed($displayReport)
+    {
+        $errMsg = "";
+        if ("" !== $_SESSION['MDWS_Type']) {
+            $errMsg .= "MDWS Type: ".$_SESSION['MDWS_Type']."; ";
+        }
+        if ("" !== $_SESSION['MDWS_Msg']) {
+            $errMsg .= "MDWS Msg: ".$_SESSION['MDWS_Msg']."; ";
+        }
+        if ("" !== $_SESSION['MDWS_Suggestion']) {
+            $errMsg .= "MDWS Suggestion: ".$_SESSION['MDWS_Suggestion']."; ";
+        }
+        if ("" === $errMsg) {
+            $errMsg .= "Unknown MDWS Error; No further details to report";
+        }
+
+        return ($errMsg);
+        /********
             if ("" !== $_SESSION['MDWS_Status'] && $displayReport) {
                     echo "<table border=1>";
                     echo "<tr><th colspan=2><h1>MDWs Crashed</h1></th></tr>";
                     echo "<tr><th>MDWs Error</th><td>" . $_SESSION['MDWS_Msg'] . "</td></tr>";
                     echo "<tr><th>Suggestion</th><td>" . $_SESSION['MDWS_Suggestion'] . "</td></tr>";
             }
-		 ********/
+         ********/
     }
-    
-    function objectToArray($d) {
-            if (is_object($d)) {
-                    // Gets the properties of the given object
+
+    public function objectToArray($d)
+    {
+        if (is_object($d)) {
+            // Gets the properties of the given object
                     // with get_object_vars function
                     $d = get_object_vars($d);
-            }
+        }
 
-            if (is_array($d)) {
-                    /*
+        if (is_array($d)) {
+            /*
                     * Return array converted to object
                     * Using __FUNCTION__ (Magic constant)
                     * for recursive call
                     */
                     return array_map(__FUNCTION__, $d);
-            }
-            else {
-                    // Return array
+        } else {
+            // Return array
                     return $d;
-            }
+        }
     }
-    
-    function MDWS_Login_Check($AccessCode,$VerifyCode) {
-            //set variables
-			$sitelist = $_SESSION['sitelist'];
-			$AC = $_SESSION['AC'];
-			$VC = $_SESSION['VC'];
-            $_SESSION['MDWS_Status'] = '';
-            $_SESSION['MDWS_Type'] = '';
-            $_SESSION['MDWS_Msg'] = '';
-            $_SESSION['MDWS_Suggestion'] = '';
 
+    public function MDWS_Login_Check($AccessCode, $VerifyCode)
+    {
+        //set variables
+            $sitelist = $_SESSION['sitelist'];
+        $AC = $_SESSION['AC'];
+        $VC = $_SESSION['VC'];
+        $_SESSION['MDWS_Status'] = '';
+        $_SESSION['MDWS_Type'] = '';
+        $_SESSION['MDWS_Msg'] = '';
+        $_SESSION['MDWS_Suggestion'] = '';
 
-            try {
-                    $client = new SoapClient("http://".$_SESSION['mdws']."/EmrSvc.asmx?WSDL");
-                    if (isset($client->fault)) {
-                            $this->MDWsCrashReport($client, "SoapClient", false);
-                            return null;
-                    }
+        try {
+            $client = new SoapClient("http://".$_SESSION['mdws']."/EmrSvc.asmx?WSDL");
+            if (isset($client->fault)) {
+                $this->MDWsCrashReport($client, "SoapClient", false);
 
-					$addDataSource = $client->addDataSource(array('id'=>'355','name'=>'vaphsdb04','datasource'=>'54.243.40.32','port'=>'9210','modality'=>'HIS','protocol'=>'VISTA','region'=>'355'));
-									
-                    if (isset($connect->connectResult->fault)) {
-                            $this->MDWsCrashReport($connect->connectResult, "Connect", false);
-                            return null;
-                    }
-                    $connect = $client->connect(array('sitelist'=>$sitelist));
-                    if (isset($connect->connectResult->fault)) {
-                            $this->MDWsCrashReport($connect->connectResult, "Connect", false);
-                            return null;
-                    }
-					
-                    $login = $client->login(array('username'=>$AC,'pwd'=>$VC,'context'=>''));
-                    if (isset($login->loginResult->fault)) {
-                            $this->MDWsCrashReport($login->loginResult, "Login", false);
-                            return null;
-                    }
-
-                    if (isset($client->fault)) {
-                            $this->MDWsCrashReport($client, "Client", false);
-                            return null;
-                    }
-                    
-                    return $client;
-                    
-            } catch (Exception $e) {
-                // echo ("Error: $e->getMessage()");
+                return;
             }
-            return null;
+
+            $addDataSource = $client->addDataSource(array('id' => '355', 'name' => 'vaphsdb04', 'datasource' => '54.243.40.32', 'port' => '9210', 'modality' => 'HIS', 'protocol' => 'VISTA', 'region' => '355'));
+
+            if (isset($connect->connectResult->fault)) {
+                $this->MDWsCrashReport($connect->connectResult, "Connect", false);
+
+                return;
+            }
+            $connect = $client->connect(array('sitelist' => $sitelist));
+            if (isset($connect->connectResult->fault)) {
+                $this->MDWsCrashReport($connect->connectResult, "Connect", false);
+
+                return;
+            }
+
+            $login = $client->login(array('username' => $AC, 'pwd' => $VC, 'context' => ''));
+            if (isset($login->loginResult->fault)) {
+                $this->MDWsCrashReport($login->loginResult, "Login", false);
+
+                return;
+            }
+
+            if (isset($client->fault)) {
+                $this->MDWsCrashReport($client, "Client", false);
+
+                return;
+            }
+
+            return $client;
+        } catch (Exception $e) {
+            // echo ("Error: $e->getMessage()");
+        }
+
+        return;
     }
-    
 }
-
-
-?>
--- framework/library/template.class.php
+++ framework/library/template.class.php
@@ -1,31 +1,34 @@
 <?php
-class Template {
+class Template
+{
+    protected $variables = array();
+    protected $_controller;
+    protected $_action;
 
-	protected $variables = array();
-	protected $_controller;
-	protected $_action;
+    public function __construct($controller, $action)
+    {
+        $this->_controller = $controller;
+        $this->_action = $action;
+    }
 
-	function __construct($controller,$action) {
-            $this->_controller = $controller;
-            $this->_action = $action;
-	}
+    /** Set Variables **/
 
-	/** Set Variables **/
+    public function set($name, $value)
+    {
+        $this->variables[$name] = $value;
+    }
 
-	function set($name,$value) {
-            $this->variables[$name] = $value;
-	}
-        
-        function get($name){
-            return $this->variables[$name];
-        }
+    public function get($name)
+    {
+        return $this->variables[$name];
+    }
 
-	/** Display Template **/
+    /** Display Template **/
 
-    function render() {
-        
+    public function render()
+    {
         extract($this->variables);
-                
+
 /*
         if (file_exists(ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'header.php')) {
                 include (ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'header.php');
@@ -35,11 +38,11 @@ class Template {
 */
         /*
          * Workaround to access controller function without requiring them to have a view.
-         * 
+         *
          */
-        if(null != $this->_action){
-			include (ROOT . DS . 'framework' . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php');			
-		}
+        if (null != $this->_action) {
+            include ROOT.DS.'framework'.DS.'application'.DS.'views'.DS.$this->_controller.DS.$this->_action.'.php';
+        }
 /*
         if (file_exists(ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'footer.php')) {
                 include (ROOT . DS . 'application' . DS . 'views' . DS . $this->_controller . DS . 'footer.php');
@@ -48,7 +51,4 @@ class Template {
         }
 */
     }
-
 }
-
-?>
