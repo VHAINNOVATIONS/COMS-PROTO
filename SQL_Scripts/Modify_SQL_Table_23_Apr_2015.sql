3fa1413d21976a8d5e41fb7ae7393e0e6830ddc6
Mike Barlow <barlowm@gmail.com>
2015-09-15 13:57:09 -0400

    Consolidation and cleanup of code prior to publishing a release.

--- SQL_Scripts/Modify_SQL_Table_23_Apr_2015.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Roles ADD Preceptor int NULL;
-ALTER TABLE Roles ADD Preceptee int NULL;
-ALTER TABLE Patient_Assigned_Templates ADD ConcurRadTherapy int NULL;
--- SQL_Scripts/Modify_SQL_Table_9_Mar_2015.sql
+++
@@ -1,7 +0,0 @@
-use COMS_TEST_6
-ALTER TABLE [Template_Regimen] ADD VistA_RouteInfo VARCHAR(100);
-ALTER TABLE [MH_Infusion] ADD VistA_RouteInfo VARCHAR(100);
-
-use COMS_TEST_7
-ALTER TABLE [Template_Regimen] ADD VistA_RouteInfo VARCHAR(100);
-ALTER TABLE [MH_Infusion] ADD VistA_RouteInfo VARCHAR(100);
