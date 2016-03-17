434db1b25bb999eee6c36f8ce3ea0cf1a1c4b3b3
Sean Cassidy <sean.cassidy@dbitpro.com>
2016-03-17 11:53:02 -0400

    Update config_Constants.php

--- app/config_Constants.php
+++ app/config_Constants.php
@@ -3,12 +3,11 @@
     define('DB_NAME', 'COMS_TEST_7');		/* For COMS-MWB App */
     // define('DB_NAME', 'COMS_TEST_6');		/* For COMS TEST App */
     define('DB_TYPE', 'sqlsrv');
-    define('DB_HOST', "DBITDATA\DBIT");
+    define('DB_HOST', "xxx");
     define('DB_USER', 'coms_db_user');
-    define('DB_PASSWORD', 'dbitPASS99');
-  // define('NV_BASE_URL', 'http://dbittest.dbitpro.com:3000/v1/');
+    define('DB_PASSWORD', 'xxx');
     define('NV_BASE_URL', 'http://localhost:3000/v1');
-    define('NV_ACCESS_TOKEN', 'dbittest');
-    define('NV_ACCESS_SECRET', 'dbitpro');
-    define('NV_ACCESS_CODE', 'CPRS1234');
-    define('NV_VERIFY_CODE', 'CPRS4321$');
+    define('NV_ACCESS_TOKEN', 'xx');
+    define('NV_ACCESS_SECRET', 'xx');
+    define('NV_ACCESS_CODE', 'xx');
+    define('NV_VERIFY_CODE', 'xx');
